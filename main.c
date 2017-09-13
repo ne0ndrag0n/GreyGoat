0d51c227ee555256ff4b40f0c0bdfc565aba7273
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2017-09-12 21:42:08 -0400

    Changeup demo

--- main.c
+++ main.c
@@ -14,11 +14,14 @@ int main() {
     gtClearFramebuffer();
 
     // Draw 10,000 pixels on screen before blitting
+    u8 color = random() % 16;
+
     for( u16 x = 0; x != 320; x++ ) {
       u8 lineLength = random() % 224;
-      u8 color = random() % 16;
 
-      for( u16 y = 0; y != lineLength; y++ ) {
+      u16 start = 112 - ( lineLength / 2 );
+      u16 end = 112 + ( lineLength / 2 );
+      for( u16 y = start; y != end; y++ ) {
         gtFramebufferPlot( x, y, color );
       }
     }
