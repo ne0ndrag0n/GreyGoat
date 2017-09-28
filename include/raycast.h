3d1dff850364dbea17620eafd43e735c2aeb6469
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2017-09-28 17:31:38 -0400

    Contiune porting example

--- include/raycast.h
+++ include/raycast.h
@@ -15,6 +15,18 @@
 #define MAP_WIDTH 24
 #define MAP_HEIGHT 24
 
+// Lookup table indexes
+#define ANGLE_60 SCREEN_X
+#define ANGLE_30 (ANGLE_60/2)
+#define ANGLE_15 (ANGLE_30/2)
+#define ANGLE_90 (ANGLE_30*3)
+#define ANGLE_180 (ANGLE_90*2)
+#define ANGLE_270 (ANGLE_90*3)
+#define ANGLE_360 (ANGLE_60*6)
+#define ANGLE_0 0
+#define ANGLE_5 (ANGLE_30/6)
+#define ANGLE_10 (ANGLE_5*2)
+
 // why the fuck isn't this in maths.h for sgdk
 typedef struct {
   fix32 x;
--- src/raycast.c
+++ src/raycast.c
@@ -8,7 +8,7 @@
 #include <math.h>
 #include <stdbool.h>
 
-const u8 worldMap[ MAP_WIDTH ][ MAP_HEIGHT ] = {
+const u8 worldMap[ MAP_HEIGHT ][ MAP_WIDTH ] = {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
   {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -35,14 +35,73 @@ const u8 worldMap[ MAP_WIDTH ][ MAP_HEIGHT ] = {
   {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
-static Vect2D_u16 position = { 96, 96 };
-static u16 direction = 0;
+static Vect2D_s16 position = { 96, 96 };
+static s16 direction = ANGLE_0;
 
 void gtRaycasterFrame() {
   // todo draw background here
 
-  // Ray will draw from directionAngle + 30 to directionAngle - 30 (60 degree fov)
+  s16 ray = direction - 30;
+  if( ray < 0 ) {
+    ray = ANGLE_360 + ray;
+  }
+
+  Vect2D_s16 grid = { 0, 0 };
+  Vect2D_s16 gridIndex = { 0, 0 };
+  Vect2D_s16 distToNextGrid = { 0, 0 };
+  Vect2D_f32 nextIntersectionDistance = { FIX32( 0.0 ), FIX32( 0.0 ) };
+  Vect2D_f32 intersection = { FIX32( 0.0 ), FIX32( 0.0 ) };
+  Vect2D_f32 distToGridBeingHit = { FIX32( 0.0 ), FIX32( 0.0 ) };
+
   for( u16 column = 0; column != SCREEN_X; column++ ) {
+    if( ray > ANGLE_0 && ray < ANGLE_180 ) {
+      // Ray faces down
+      grid.x = ( position.y / R_TILE_SIZE ) * R_TILE_SIZE + R_TILE_SIZE;
+      distToNextGrid.x = R_TILE_SIZE;
+
+      intersection.x = fix32Div( FIX32( grid.x - position.y ), tanLookup[ ray ] );
+      intersection.x = fix32Add( intersection.x, FIX32( position.x ) );
+    } else {
+      // Ray faces up
+      grid.x = ( position.y / R_TILE_SIZE ) * R_TILE_SIZE;
+      distToNextGrid.x = -R_TILE_SIZE;
+
+      intersection.x = fix32Div( FIX32( grid.x - position.y ), tanLookup[ ray ] );
+      intersection.x = fix32Add( intersection.x, FIX32( position.x ) );
+
+      grid.x--;
+    }
+
+    if( ray == ANGLE_0 || ray == ANGLE_180 ) {
+      distToGridBeingHit.x = FIX32( 2097151.0 );
+    } else {
+      nextIntersectionDistance.x = fix32Div( FIX32( R_TILE_SIZE ), tanLookup[ ray ] );
+      if( nextIntersectionDistance.x < FIX32( 0.0 ) ) {
+        nextIntersectionDistance.x = -nextIntersectionDistance.x;
+      }
+
+      while( true ) {
+        gridIndex.x = fix32ToInt( fix32Div( intersection.x, FIX32( R_TILE_SIZE ) ) );
+        gridIndex.y = grid.x / R_TILE_SIZE;
+
+        if(
+          ( gridIndex.x >= MAP_WIDTH ) ||
+          ( gridIndex.y >= MAP_HEIGHT ) ||
+          ( gridIndex.x < 0 ) ||
+          ( gridIndex.y < 0 )
+        ) {
+          distToGridBeingHit.x = FIX32( 2097151.0 );
+          break;
+        } else if ( worldMap[ gridIndex.y ][ gridIndex.x ] ) {
+          distToGridBeingHit.x = fix32Sub( intersection.x, FIX32( position.x ) );
+          distToGridBeingHit.x = fix32Div( distToGridBeingHit.x, cosLookup[ ray ] );
+          break;
+        } else {
+          intersection.x = fix32Add( intersection.x, nextIntersectionDistance.x );
+          grid.x += distToNextGrid.x;
+        }
+      }
+    }
 
   }
 
